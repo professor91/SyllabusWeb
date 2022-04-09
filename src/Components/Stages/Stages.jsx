c61ac88c3027f8d42b4af85e2a0db1f6c0d6c2d7
professor91 <keshavcode3@gmail.com>
2022-04-09 12:28:21 +0530

    programme list loading from json

--- src/Components/Stages/Stages.jsx
+++ src/Components/Stages/Stages.jsx
@@ -1,31 +1,69 @@
+/* 
+    Page which contains list of all {UG, PG, PhD, Certificate & Diploma programmes
+
+    ProgrammeGrid component
+                            final (list)
+                                contains list of <a> tag where
+                                    each <a> tag links to the main page of the programme
+                                
+                                href structure
+                                    /{ug,pg,phd,certificate,diplome}/{name_of_the_programme}
+*/
+
 import "./Stages.css"
 
 const ProgrammesGrid= (props) => {
-    const programmes= props.programmes
-    const final= []
-    
-    for (let programme of programmes){
-        final.push(
-            <div 
-                class="programme-grid-item">
-                <a 
-                    href="/ug/bschcs">
-                    {programme}
-                </a>
-                </div>
-        )
+    var jsonData
+
+    if (props.type=== "bsc") {
+        jsonData= require('../../database/programmes/ug/bsc.json')
     }
-    
+    else if (props.type=== "ba") {
+        jsonData= require('../../database/programmes/ug/ba.json')
+    }
+    else if (props.type=== "balang") {
+        jsonData= require('../../database/programmes/ug/balang.json')
+    }
+    else if (props.type=== "comm") {
+        jsonData= require('../../database/programmes/ug/comm.json')
+    }
+
+    else if (props.type=== "msc") {
+        jsonData= require('../../database/programmes/pg/msc.json')
+    }
+    else if (props.type=== "ma") {
+        jsonData= require('../../database/programmes/pg/ma.json')
+    }
+    else if (props.type=== "malang") {
+        jsonData= require('../../database/programmes/pg/malang.json')
+    }
+    else if (props.type=== "mcomm") {
+        jsonData= require('../../database/programmes/pg/comm.json')
+    }
+
+    const final= jsonData["Courses"]
+
     return(
-        // List of All Courses 
         <div class="container">
             <div class="programme-grid-header"> {props.heading} </div>
             <div class="programme-grid">
-                {final}
+                {/* List of All Courses */}
+                {
+                    final.map(
+                        prog => 
+                            <div 
+                                class="programme-grid-item">
+                                <a 
+                                    href={"/programmes/ug/"+prog[1]}>
+                                    {prog[0]}
+                                </a>
+                            </div>
+                    )
+                }
             </div>
         </div>
     );
 
 }
 
-export default ProgrammesGrid
+export default ProgrammesGrid;
