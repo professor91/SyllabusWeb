19ab1bf45ee7b00a97414e23ac1a8b1bc8706cef
professor91 <keshavcode3@gmail.com>
2022-06-22 15:27:10 +0530

    added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
-  // <BrowserRouter>
-    <App />,
-  // </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />,
+    </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
