43f7a7ac668038bf80d2e61624fd1e50f7bcb509
professor91 <keshavcode3@gmail.com>
2022-06-29 19:41:33 +0530

    remove browserouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />,
-    </BrowserRouter>,
   </React.StrictMode>,
   document.getElementById('root')
 );
